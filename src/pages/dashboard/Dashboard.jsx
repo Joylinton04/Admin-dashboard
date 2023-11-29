e8bc4e8a17fbbfa4e585b687e8767e6116ee4915
JoyLinton04 <adujoy05@gmail.com>
2023-11-29 21:10:59 +0000

    Added content to the dashboard page

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <div>dashboard</div>
+    <Box m="20px ">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Header title="DASHBOARD" subtitle="Welcome to you dashboard"/> 
+        </Box>
+    </Box>
   )
 }
 
