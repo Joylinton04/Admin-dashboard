33bf0c5551800e859ce33705d1e905f3d33a0ece
JoyLinton04 <adujoy05@gmail.com>
2023-11-29 23:37:27 +0000

    created pie page

--- src/pages/dashboard/Pie.jsx
+++ src/pages/dashboard/Pie.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
 
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart"/>
+      <Box height="75vh">
+          <PieChart/>
+      </Box>
+    </Box>
   )
 }
 
-export default Pie;
+export default Pie; 
